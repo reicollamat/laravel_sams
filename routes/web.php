7a703c0d326dbaf87ec3983645ea5db02438963f
richmondb <richmond.billones@gmail.com>
2023-02-07 23:10:04 +0800

    added new welcome

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 // Welcome/Home View
 Route::get('/', function () {
-    return view('index');
+    return view('security.index');
 });
 // signed in user view
 Route::get('/dashboard', function () {
