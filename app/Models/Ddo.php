dd1da0b2ec47faf679db7fb99660d4aa260c4ba9
raycollamat <raycollamat11@gmail.com>
2023-02-07 23:29:21 +0800

    model&controller files

---
+++ app/Models/Ddo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ddo extends Model
+{
+    use HasFactory;
+}
