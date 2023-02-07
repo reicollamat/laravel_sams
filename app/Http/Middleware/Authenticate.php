c0a649739366527bb60f346065484f141a316aea
richmondb <richmond.billones@gmail.com>
2023-02-07 13:43:50 +0800

    fix user log in issues for non admin

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
@@ -15,6 +16,10 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
+            // logouts the current user signed in
+            Auth::logout();
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
             return route('login');
         }
     }
