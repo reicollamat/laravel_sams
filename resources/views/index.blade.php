10d2f01297171a5df35987eff5ed379eab795887
richmondb <richmond.billones@gmail.com>
2023-02-07 23:06:49 +0800

    added new welcome

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,14 +5,18 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>{{ env('APP_NAME') }}</title>
 
+    {{-- icon --}}
+    <link rel="icon" type="image/x-icon" href="{{ URL::asset('img/icon.svg') }}">
+    
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ URL::asset('bootstrap/css/bootstrap.min.css') }}" />
-
+    <link rel="stylesheet" href="{{ URL::asset('css/main.css') }}"  type="text/css">
+    <link rel="stylesheet" href="{{ URL::asset('fontawesome-6-2-1/all.min.css') }}"  type="text/css">
 </head>
 
 <body class="antialiased">
@@ -26,6 +30,8 @@
     <p>hello</p>
     {{-- js script --}}
     <script src="{{ URL::asset('bootstrap/js/bootstrap.bundle.min.js') }}" async></script>
+    <script src="{{ URL::asset('js/main.js') }}" async></script>
+
 </body>
 
 </html>
