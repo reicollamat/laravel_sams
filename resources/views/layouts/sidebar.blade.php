fedef3ecb65420a4809bb0767310814375a2e25f
raycollamat <raycollamat11@gmail.com>
2023-02-09 01:26:46 +0800

    fix conflict

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -100,8 +100,8 @@
    </div>
 </aside>
 
-{{-- 
-bodylayout
+
+{{-- bodylayout --}}
 <div class="p-4 sm:ml-64">
    <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
       <div class="grid grid-cols-3 gap-4 mb-4">
@@ -150,4 +150,4 @@ bodylayout
          </div>
       </div>
    </div>
-</div> --}}
+</div>
