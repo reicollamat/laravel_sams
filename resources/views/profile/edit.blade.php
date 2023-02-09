62ff079a5a08be757466976b53f3ece76a04654a
richmondb <richmond.billones@gmail.com>
2023-02-09 23:36:04 +0800

    fix bug

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,4 +1,5 @@
-<x-adminapp-layout>
+@extends('layouts.adminapp')
+@section('content')
     <div class="p-4 sm:ml-64">
         <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
             <x-slot name="header">
@@ -31,4 +32,4 @@
 
         </div>
     </div>
-</x-adminapp-layout>
+@endsection
