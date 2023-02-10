6684a598796924c187bd06403cfdf4c1c8339aa1
richmondb <richmond.billones@gmail.com>
2023-02-10 17:57:50 +0800

    remove cdn flowbite added as module

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
-
+    darkMode:'media',
     theme: {
         extend: {
             fontFamily: {
@@ -16,5 +16,8 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('flowbite/plugin'),
+    ],
 };
