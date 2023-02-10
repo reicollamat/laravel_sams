6d6b3da23f15ce9c05e38e40f639b469e2e6b3aa
richmondb <richmond.billones@gmail.com>
2023-02-11 01:52:47 +0800

    experimental alert

--- resources/views/admintab/addsecurity/addguard.blade.php
+++ resources/views/admintab/addsecurity/addguard.blade.php
@@ -4,7 +4,29 @@
 <div class="p-4 sm:ml-64 mt-14">
     <div class="p-3 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-7">
         
-        @foreach ($errors->all() as $error)
+
+        @if (session('status') === 'Guards Added!') 
+        <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 5000)"
+            class="text-sm text-gray-600 dark:text-gray-400 fade">{{ __('Saved.') }}</p>
+            <script>
+                alert('Guards Added!');
+            </script> 
+        @endif
+        <div x-data="{ open: false }">
+            <button @click="open = ! open">Expand</button>
+         
+            <div x-show="open">
+                Content...
+            </div>
+        </div>
+        <div x-data="{ open: false }">
+            <button @click="open = ! open">Expand</button>
+         
+            <div x-show="open">
+                Content...
+            </div>
+        </div>
+        {{-- @foreach ($errors->all() as $error)
             <div id="alert-border-5" class="flex p-4 border-t-4 border-gray-300 bg-gray-50 dark:bg-gray-800 dark:border-gray-600" role="alert">
                 <svg class="flex-shrink-0 w-5 h-5 text-gray-800 dark:text-gray-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
                 <div class="ml-3 text-sm font-medium text-gray-800 dark:text-gray-300">
@@ -15,7 +37,7 @@
                     <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </button>
             </div>
-        @endforeach
+        @endforeach --}}
         
         <form method="POST" action="/securityguard/add/">
             @csrf
