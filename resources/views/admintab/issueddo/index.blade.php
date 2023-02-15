ee3ae9229472a30d39a9e3cfc4df3127536d0aaf
richmondb <richmond.billones@gmail.com>
2023-02-16 00:30:06 +0800

    add pages for admin tabs

--- resources/views/admintab/issueddo/index.blade.php
+++ resources/views/admintab/issueddo/index.blade.php
@@ -1,16 +1,14 @@
-@extends('layouts.mainlayout')
+@extends('layouts.masterapp')
 @section('content')
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Contract Requests') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    Issue DDO here...
+    <div class="p-4 sm:ml-64 mt-14">
+        <div class="p-3 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-7">
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900 dark:text-gray-100">
+                            Issue DDO Table here...
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
